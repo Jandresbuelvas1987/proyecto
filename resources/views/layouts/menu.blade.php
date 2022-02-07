93d183378ccf106c8c5c94ebb649a02b37d8fac5
jandresbuelvas <dev4@dyalogo.com>
2022-02-07 16:46:45 -0500

    inicializacion de proyecto

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,15 @@
+<li class="side-menus {{ Request::is('*') ? 'active' : '' }}">
+    <a class="nav-link" href="/home">
+        <i class=" fas fa-archway"></i><span>Inicio</span>
+    </a>
+    <a class="nav-link" href="/usuarios">
+        <i class=" fas fa-users"></i><span>Usuarios</span>
+    </a>
+    <a class="nav-link" href="/roles">
+        <i class=" fas fa-user-lock"></i><span>Roles</span>
+    </a>
+    <a class="nav-link" href="/blogs">
+        <i class=" fas fa-blog"></i><span>Blogs</span>
+    </a>
+</li>
+ 
